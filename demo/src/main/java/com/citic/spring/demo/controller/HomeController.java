7c1e847944a518be69b7359f054704cf0974750e
tzg <tianzg3@citic.com>
2021-02-03 16:28:13 +0800

    init

---
+++ demo/src/main/java/com/citic/spring/demo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.citic.spring.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class HomeController {
+ 
+    @RequestMapping("/")
+    public String home(){
+        return "你好，欢迎使用Visual Studio Code!";
+    }
+    @RequestMapping("/my")
+    public String homeMy(){
+        return "你好，欢迎使用Visual Studio Code! it is my";
+    }
+}
